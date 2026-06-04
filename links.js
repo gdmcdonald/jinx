46474e1ebebfca7e04e5d3e32b7038adcdc9056f
Gordon McDonald <gordon27@gmail.com>
2026-06-04 23:47:24 +1000

    add links

--- links.js
+++ links.js
@@ -4,13 +4,29 @@ const REDIRECTS = {
     url: 'https://github.com/gdmcdonald/jinx',
     description: 'GitHub repository for Jinx'
   },
+  'analytics': {
+    url: 'https://www.goatcounter.com/',
+    description: 'Goat Counter for page vieew tracking'
+  },
   'important': {
     url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
     description: 'Important things'
   },
-  'landscape': {
+  'competition': {
     url: 'https://github.com/topics/url-shortener',
     description: 'Other Url Shorteners you could self-host'
+  },
+  'qr': {
+    url: 'https://www.nayuki.io/page/qr-code-generator-library',
+    description: 'QR Code generator library by Nayuki which makes the QR codes on the right ->'
+  },
+  'fuzzy-search': {
+    url: 'https://github.com/lucaong/minisearch',
+    description: 'Minisearch js library - powers the fuzzy search above ^'
+  },
+  'inspired-by': {
+    url: 'https://github.com/nelsontky/gh-pages-url-shortener',
+    description: 'Jinx was inspired by this other link shortener that used 404 redirects and github issues'
   },
 };
 
